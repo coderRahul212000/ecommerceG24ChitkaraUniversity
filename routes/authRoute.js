271d3069e2298924744b6064218c8acfb6b51448
coderRahul212000 <rahulrajput211998@gmail.com>
2025-01-28 17:36:42 +0530

    fourthclass tuesday class

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,14 @@
 import express from "express"
-import {registerController, loginController} from "../controllers/authController.js"
+import {registerController, loginController, testController} from "../controllers/authController.js"
+
+import { requireSignIn, isAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerController)
 
 router.post("/login", loginController)
+
+router.get("/test", requireSignIn ,isAdmin, testController)
 
 export default router
