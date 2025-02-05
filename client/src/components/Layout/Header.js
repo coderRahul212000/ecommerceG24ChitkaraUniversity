c1e8066f5a86f3a3152758ec908c073aa4ec038b
coderRahul212000 <rahulrajput211998@gmail.com>
2025-02-05 21:05:41 +0530

    5th feb  2025

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
+import { FaBagShopping } from "react-icons/fa6";
+
 
 const Header = () => {
   return (
@@ -10,20 +12,26 @@ const Header = () => {
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <NavLink to="/" className="navbar-brand" href="#">Hidden brand</NavLink>
+      <Link to="/" className="navbar-brand" href="#">
+
+
+     🛒 Ecommerce App</Link>
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <NavLink to="/" className="nav-link active" aria-current="page" href="#">Home</NavLink>
+          <NavLink to="/" className="nav-link " >Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/category" className="nav-link " >Category</NavLink>
         </li>
        
         <li className="nav-item">
-          <NavLink to="/register" className="nav-link" href="#">Register</NavLink>
+          <NavLink to="/register" className="nav-link" >Register</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/login" className="nav-link" href="#">Login</NavLink>
+          <NavLink to="/login" className="nav-link" >Login</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/cart" className="nav-link" href="#">Cart(0)</NavLink>
+          <NavLink to="/cart" className="nav-link" >Cart(0)</NavLink>
         </li>
        
       </ul>
